25b4e811a63811c988c8f5825d2f3faae11fc58d
Dipali gunjal <143181151+Dipalig971@users.noreply.github.com>
2023-12-21 19:11:36 +0500

    22.12.23

---
+++ operator/Arithmetic.dart
@@ -0,0 +1,13 @@
+void main()
+{
+   int a=20;
+   int b=10;
+
+  //  int sum = a+b;
+
+   print("the sum of $a and $b is : ${a+b}");
+   print("the substraction of $a and $b is : ${a-b}");
+   print("the multiplication of $a and $b is : ${a*b}");
+   print("the divide of $a and $b is : ${a/b}");
+   
+}
